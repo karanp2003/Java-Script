7f645506bd26502c2f9c2cdf32207fde9af2c6eb
karanp2003 <karan.9001462891@gmail.com>
2026-05-22 06:42:40 +0000

    done

---
+++ basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=12332
+var accountPassword="12345"
+let accountEmail="account@gmail.com"
+
+let accountCity="Jaipur"
+
+let accountState;
+accountEmail="hc@gmail.com"
+accountPassword="1222"
+accountCity="Bengaluru"
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use Strict";
+
+console.log(3+3)
+
+console.log("karan")
